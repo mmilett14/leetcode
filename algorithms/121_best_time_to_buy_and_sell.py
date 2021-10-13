a0aa18fc4000089c999c685fef08561d4dad83cd
mike <78767272+mmilett14@users.noreply.github.com>
2021-10-13 09:18:41 -0500

    brute-force solution

--- algorithms/121_best_time_to_buy_and_sell.py
+++ algorithms/121_best_time_to_buy_and_sell.py
@@ -1,30 +1,22 @@
-# Need to address cases where the price is listed more than once
+# https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
 
-class Solution:
-    def maxProfit(self, prices: List[int]) -> int:
-        
-        prices_asc = prices.copy()
-        prices_asc.sort(reverse=False)
-        
-        prices_desc = prices.copy()
-        prices_desc.sort(reverse=True)
-        
-        price_dict = {}
+class Solution(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
         
         profit = 0
+        max_profit = 0
         
         for i in range(0, len(prices)):
-            
-            price_dict[prices[i]] = i
-            
-        
-        for i in range (0, len(prices)):
-            
-            for j in range (0, len(prices)):
-            
-                if (price_dict[prices_desc[i]] > price_dict[prices_asc[j]]) and ((prices_desc[i] - prices_asc[j]) > profit):
-                    profit = (prices_desc[i] - prices_asc[j])
-            
-        return profit
-        
+            for j in range(i+1, len(prices)):
+                
+                profit = prices[j] - prices[i]
+                
+                if profit > max_profit:
+                    max_profit = profit
+                    
+        return max_profit
         
