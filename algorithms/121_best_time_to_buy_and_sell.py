d7a4105575cb0cb74d36acbd27c58f85304b516c
mike <78767272+mmilett14@users.noreply.github.com>
2021-10-13 09:31:14 -0500

    onepass solution

--- algorithms/121_best_time_to_buy_and_sell.py
+++ algorithms/121_best_time_to_buy_and_sell.py
@@ -7,16 +7,15 @@ class Solution(object):
         :rtype: int
         """
         
-        profit = 0
+        min_price = float('inf')
         max_profit = 0
         
         for i in range(0, len(prices)):
-            for j in range(i+1, len(prices)):
-                
-                profit = prices[j] - prices[i]
-                
-                if profit > max_profit:
-                    max_profit = profit
-                    
-        return max_profit
+            
+            if prices[i] < min_price:
+                min_price = prices[i]
+            
+            elif prices[i] - min_price > max_profit:
+                max_profit = prices[i] - min_price
         
+        return max_profit
