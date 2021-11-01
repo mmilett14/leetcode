c1d9ff807ebc499e82a806b576d1bb2608c68f5c
mike <78767272+mmilett14@users.noreply.github.com>
2021-11-01 08:43:53 -0500

    brute force method

---
+++ algorithms/278_first_bad_version.py
@@ -0,0 +1,17 @@
+# https://leetcode.com/problems/first-bad-version/
+
+# The isBadVersion API is already defined for you.
+# @param version, an integer
+# @return a bool
+# def isBadVersion(version):
+
+class Solution(object):
+    def firstBadVersion(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        
+        for version in range(n+1):
+            if isBadVersion(version):
+                return version
