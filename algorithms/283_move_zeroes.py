e2a29ae178bfa665757c6ed4ec9203538d1c3820
mike <78767272+mmilett14@users.noreply.github.com>
2021-11-10 15:08:13 -0600

    solution from site

--- algorithms/283_move_zeroes.py
+++ algorithms/283_move_zeroes.py
@@ -1,15 +1,17 @@
+# https://leetcode.com/problems/move-zeroes/
+
 class Solution:
     def moveZeroes(self, nums: List[int]) -> None:
         """
         Do not return anything, modify nums in-place instead.
         """
         
-        nums_no_zeros = nums.copy()
-        nums_no_zeros[:] = [num for num in nums if num != 0]
-
-        zero_cnt = len(nums) - len(nums_no_zeros)
-
-        nums = nums_no_zeros.copy()
-
-        for zero in range(zero_cnt):
-            nums.append(0)
+        lastNonZeroFoundAt = 0
+        
+        for num in nums:
+            if num != 0:
+                nums[lastNonZeroFoundAt] = num
+                lastNonZeroFoundAt += 1
+        
+        for i in range(lastNonZeroFoundAt, len(nums)):
+            nums[i] = 0
